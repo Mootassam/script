dd7d89ffeae5b02f8052b59a70920f9d5a01c335
Mootassam <mootassame@gmail.com>
2023-07-21 16:36:44 -0400

    change the color when clikc

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,129 +1,48 @@
 import "../Content/content.css";
-
 import { AiOutlineCopy } from "react-icons/ai";
 import { TfiReload } from "react-icons/tfi";
+import { useEffect, useState } from "react";
+import editor5 from "../../data/editor5.json";
 
 function Content() {
+  const [current, setCurrent] = useState<number[]>([]);
+
+  const toggleSelection = (index: number) => {
+    if (current.includes(index)) {
+      setCurrent((prev) => prev.filter((item) => item !== index));
+    } else {
+      setCurrent((prev) => [...prev, index]);
+    }
+  };
+
+  useEffect(() => {
+  }, [current]);
+
   return (
     <div className="app__content">
       <div className="content">
         <div className="content__">
-          <div className="content__detail">
-            <span> 1. I will choose a quota of 5000.</span>
-            <span className="copy__">
-            <TfiReload size="24"  />
-              <AiOutlineCopy size="25" color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 2. i participate with 2000.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"   color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 3. quota for 9000.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 4. I am excited to do this 5000.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 5. I will make 2000 investment.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span>
-              {" "}
-              6. Already contacted my receptionist to book qouta of rs15000.
-            </span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-
-          <div className="content__detail">
-            <span> 7. I do 9000 in this task.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 8. I will choose a quota of 5000.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 9. I will choose 2000 to complete this session.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 10. Kindly reserve me a quota for this task 9000.</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 11. Hello, I want to invest 2000 now</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 12. she give me this quota and i choose Rs5000</span>
-            <span className="copy__">
-            <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-
-          <div className="content__detail">
-            <span> 13. I have booked for a mission quota of 15000</span>
-            <span className="copy__">
-              {" "}
-              <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span>
-              {" "}
-              14. still choosing the quota i want to participate with 2000
-            </span>
-            <span className="copy__">
-              {" "}
-              <TfiReload size="24" />
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
-          <div className="content__detail">
-            <span> 15. I also upgraded my quota to 9000 receptionist.</span>
-            <span className="copy__">
-              <TfiReload size="24" />
-
-              <AiOutlineCopy size="25"  color="#871787" />
-            </span>
-          </div>
+          {editor5.map((item, index) => (
+            <div
+              className={`content__detail ${
+                current.includes(index) ? "__background" : ""
+              }`}
+              key={index}
+            >
+              <span className="">
+                {index + 1}) {item.script}
+              </span>
+              <span className="copy__">
+                <div>
+                  <TfiReload size="24" />
+                </div>
+                <div onClick={() => toggleSelection(index)}>
+                  {" "}
+                  <AiOutlineCopy size="25" color="#871787" />
+                </div>
+              </span>
+            </div>
+          ))}
         </div>
         <div className="generate__">
           <button className="button__generate">Generate</button>
